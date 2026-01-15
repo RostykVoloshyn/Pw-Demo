8828addfcfbdd75c683a8c3977aeb64355a7f3a9
VRostysav <df6g78s@gmail.com>
2026-01-15 18:50:03 +0200

    Modified the workflow

--- pw-practice-app/playwright.config.ts
+++ pw-practice-app/playwright.config.ts
@@ -21,6 +21,13 @@ export default defineConfig<TestOptions>({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+
+   webServer: {
+    command: 'npm start',
+    url: 'http://localhost:4200',
+    timeout: 120 * 1000,
+    reuseExistingServer: false,
+  },
   reporter: [
     ["html"], 
     ["allure-playwright",{
